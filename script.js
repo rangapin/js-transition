b9e7d8cb282cdabc716e0a3e9e4aa759f2e2deaf
Richard Angapin <rangapin@gmx.com>
2020-04-29 14:57:45 +0200

    git init

---
+++ script.js
@@ -0,0 +1,8 @@
+new hoverEffect ({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: 'pic-one.jpg',
+    image2: 'pic-two.jpg',
+    angle: Math.PI / 4,
+    displacementImage:  'fluid.jpg'
+});
